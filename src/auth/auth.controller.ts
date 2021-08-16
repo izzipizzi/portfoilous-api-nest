597b1e461607eb9458735eb08fb017344aff3f2d
Andrii Kirchei <kircheiandri@gmail.com>
2021-08-16 17:35:23 +0300

    eslint fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,33 +1,34 @@
 import {
-  Body,
-  Controller,
-  HttpException,
-  HttpStatus,
-  Post,
+    Body,
+    Controller,
+    HttpException,
+    HttpStatus,
+    Post,
 } from '@nestjs/common';
-import { AuthService, RegistrationStatus } from './auth.service';
-import { CreateUserRequestDto } from '../dto/CreateUser.dto';
-import { LoginUserDto } from '../dto/LoginUser.dto';
+import {AuthService, RegistrationStatus} from './auth.service';
+import {CreateUserRequestDto} from '../dto/CreateUser.dto';
+import {LoginUserDto} from '../dto/LoginUser.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-
-  @Post('register')
-  public async register(
-    @Body() createUserDto: CreateUserRequestDto,
-  ): Promise<RegistrationStatus> {
-    const result: RegistrationStatus = await this.authService.register(
-      createUserDto,
-    );
-    if (!result.success) {
-      throw new HttpException(result.message, HttpStatus.BAD_REQUEST);
+    constructor(private readonly authService: AuthService) {
     }
-    return result;
-  }
 
-  @Post('login')
-  public async login(@Body() loginUserDto: LoginUserDto): Promise<any> {
-    return await this.authService.login(loginUserDto);
-  }
+    @Post('register')
+    public async register(
+        @Body() createUserDto: CreateUserRequestDto,
+    ): Promise<RegistrationStatus> {
+        const result: RegistrationStatus = await this.authService.register(
+            createUserDto,
+        );
+        if (!result.success) {
+            throw new HttpException(result.message, HttpStatus.BAD_REQUEST);
+        }
+        return result;
+    }
+
+    @Post('login')
+    public async login(@Body() loginUserDto: LoginUserDto): Promise<any> {
+        return await this.authService.login(loginUserDto);
+    }
 }
